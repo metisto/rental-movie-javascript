7828f996b2f9548c6afe051334a87b97859d7259
Olivier Albiez <olivier.albiez@poloper.org>
2016-12-28 22:52:49 +0100

    Add test and simplify infrastructure

--- src/__tests__/rental-test.js
+++ src/__tests__/rental-test.js
@@ -1,9 +1,23 @@
 
 import { statement } from 'rental';
+import expect from 'expect';
 
 describe('statement', () => {
+
+  const movies = {
+    'F001': { 'title': 'Star Wars', 'code': 'regular' },
+    'F002': { 'title': 'Assasin\'s creed', 'code': 'new' },
+    'F003': { 'title': 'Bambi', 'code': 'childrens' },
+  };
+
   it('should blah', () => {
-  });
-  it('should blah 2', () => {
+    const customer = {
+      'name': 'martin',
+      'rentals': [
+        { 'movieID': 'F001', 'days': 3 },
+        { 'movieID': 'F002', 'days': 1 },
+      ],
+    };
+    expect(statement(customer, movies)).toEqual('Rental Record for martin\n\tStar Wars\t3.5\n\tAssasin\'s creed\t3\nAmount owed is 6.5\nYou earned 2 frequent renter points\n');
   });
 });
