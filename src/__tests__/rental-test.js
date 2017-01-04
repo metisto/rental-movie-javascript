e613427c8a8e21208ae1d6d1a273b40bc11f37e0
Olivier Albiez <olivier.albiez@poloper.org>
2017-01-04 18:23:59 +0100

    Update statement test

--- src/__tests__/rental-test.js
+++ src/__tests__/rental-test.js
@@ -4,20 +4,49 @@ import expect from 'expect';
 
 describe('statement', () => {
 
-  const movies = {
-    'F001': { 'title': 'Star Wars', 'code': 'regular' },
-    'F002': { 'title': 'Assasin\'s creed', 'code': 'new' },
-    'F003': { 'title': 'Bambi', 'code': 'childrens' },
-  };
+  it('should be able to rent regular movies', () => {
+    const movies = {
+      'F001': { 'title': 'Regular movie', 'code': 'regular' },
+    };
 
-  it('should blah', () => {
     const customer = {
       'name': 'Jhon Doe',
       'rentals': [
-        { 'movieID': 'F001', 'days': 3 },
-        { 'movieID': 'F002', 'days': 1 },
+        { 'movieID': 'F001', 'days': 2 },
+        { 'movieID': 'F001', 'days': 5 },
       ],
     };
-    expect(statement(customer, movies)).toEqual('Rental Record for Jhon Doe\n\tStar Wars\t3.5\n\tAssasin\'s creed\t3\nAmount owed is 6.5\nYou earned 2 frequent renter points\n');
+    expect(statement(customer, movies)).toEqual('Rental Record for Jhon Doe\n\tRegular movie\t2\n\tRegular movie\t6.5\nAmount owed is 8.5\nYou earned 2 frequent renter points\n');
   });
+
+  it('should be able to rent childrens movies', () => {
+    const movies = {
+      'F001': { 'title': 'Childrens movie', 'code': 'childrens' },
+    };
+
+    const customer = {
+      'name': 'Jhon Doe',
+      'rentals': [
+        { 'movieID': 'F001', 'days': 2 },
+        { 'movieID': 'F001', 'days': 5 },
+      ],
+    };
+    expect(statement(customer, movies)).toEqual('Rental Record for Jhon Doe\n\tChildrens movie\t1.5\n\tChildrens movie\t4.5\nAmount owed is 6\nYou earned 2 frequent renter points\n');
+  });
+
+  it('should be able to rent new movies', () => {
+    const movies = {
+      'F001': { 'title': 'New movie', 'code': 'new' },
+    };
+
+    const customer = {
+      'name': 'Jhon Doe',
+      'rentals': [
+        { 'movieID': 'F001', 'days': 2 },
+        { 'movieID': 'F001', 'days': 5 },
+      ],
+    };
+    expect(statement(customer, movies)).toEqual('Rental Record for Jhon Doe\n\tNew movie\t6\n\tNew movie\t15\nAmount owed is 21\nYou earned 3 frequent renter points\n');
+  });
+
 });
